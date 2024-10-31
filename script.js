d8dbfc6f9574e10f94afe64e61e5d717b5b9b4df
blessedliu <blessedliu@gmail.com>
2024-10-31 01:03:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutSection = document.querySelector("#about");
+    aboutSection.addEventListener("click", function() {
+        alert("Thanks for learning more about me!");
+    });
+});
